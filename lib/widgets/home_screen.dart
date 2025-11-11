59afcf29894360c1f71dddb576f82cf915aa121e
Hasan_nawaz <Hassannawaz1390@gmail.com>
2025-11-11 22:02:59 +0500

    initial commit

---
+++ lib/widgets/home_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class home_screen extends StatefulWidget {
+  const home_screen({super.key});
+
+  @override
+  State<home_screen> createState() => _home_screenState();
+}
+
+class _home_screenState extends State<home_screen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+      ),
+      body: const Center(
+        child: Text('Welcome to the Home Screen!'),
+      ),
+
+    );
+  }
+}
---
+++ lib/widgets/splash_screen.dart
@@ -0,0 +1,48 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:shared_preference/widgets/home_screen.dart';
+
+class Splash_screen extends StatefulWidget {
+  const Splash_screen({super.key});
+
+  @override
+  State<Splash_screen> createState() => _Splash_screenState();
+
+}
+
+class _Splash_screenState extends State<Splash_screen> {
+
+  void initState() {
+    super.initState();
+
+    Timer(Duration(seconds: 5), () {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => home_screen()));
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Text(
+              'Splash Screen',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            Expanded(
+              child: Image.network(
+                'https://images.pexels.com/photos/34502687/pexels-photo-34502687.png',
+                fit: BoxFit.cover,
+                width: double.infinity,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
